ab9d8256f59a0572188c703d961c5b7fe33399ee
Webdev2024 <Apoorvagowda551@gmail.com>
2023-07-05 10:26:34 +0530

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment ={
+    production :false,
+    weatherApiBaseUrl :'https://open-weather-map27.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue:'open-weather-map27.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'1131b4cbeemsh343246d6ada5bcbp133883jsn24344a852897'
+
+}
